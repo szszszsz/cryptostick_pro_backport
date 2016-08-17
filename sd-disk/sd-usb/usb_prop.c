893841f64a0a7260ca91bbf2c93232ffe5863637
Szczepan Zalega <szczepan@nitrokey.com>
2016-08-17 19:21:15 +0200

    Config descriptor guard update
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- sd-disk/sd-usb/usb_prop.c
+++ sd-disk/sd-usb/usb_prop.c
@@ -331,7 +331,7 @@ uint8_t *MASS_GetConfigDescriptor(uint16_t Length) {
 uint8_t *MASS_GetStringDescriptor(uint16_t Length) {
   uint8_t wValue0 = pInformation->USBwValue0;
 
-  if (wValue0 > 5) {
+  if (wValue0 > 4) {
     return NULL;
   } else {
     return Standard_GetDescriptorData(Length, String_Descriptor[wValue0]);
